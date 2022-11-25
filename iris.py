66e4a4874a365dda40a348f2877c1d95b0f46adf
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2022-11-25 22:31:13 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,49 +1,50 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from streamlit_option_menu import option_menu
+from apps import home, heatmap, upload  # import your app modules here
 
-st.write("""
-# Lim Jia Yee - My first app
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
+st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
-st.sidebar.header('User Input Parameters')
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+apps = [
+    {"func": home.app, "title": "Home", "icon": "house"},
+    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
+    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+]
 
-df = user_input_features()
+titles = [app["title"] for app in apps]
+titles_lower = [title.lower() for title in titles]
+icons = [app["icon"] for app in apps]
 
-st.subheader('User Input parameters')
-st.write(df)
+params = st.experimental_get_query_params()
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+if "page" in params:
+    default_index = int(titles_lower.index(params["page"][0].lower()))
+else:
+    default_index = 0
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        options=titles,
+        icons=icons,
+        menu_icon="cast",
+        default_index=default_index,
+    )
 
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
+            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
+        
+        Source code: <https://github.com/giswqs/streamlit-template>
+        More menu icons: <https://icons.getbootstrap.com>
+    """
+    )
 
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+for app in apps:
+    if app["title"] == selected:
+        app["func"]()
+        break
