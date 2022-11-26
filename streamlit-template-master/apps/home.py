4eec5781f4cc01047ff9ae71e9089e4823309ceb
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2022-11-26 20:27:40 +0800

    Update home.py

--- streamlit-template-master/apps/home.py
+++ streamlit-template-master/apps/home.py
@@ -11,5 +11,8 @@ def app():
     st.markdown(“https://app.powerbi.com/view?r=eyJrIjoiOGVhNTA1YmMtY2VhYy00Nzk4LTk3NzQtYzI4OTA5OTczNDEzIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection0f765a7eda61c7c2a3c8”, unsafe_allow_html=True)
     
     
+    m = leafmap.Map(locate_control=True)
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=700)
 
     
