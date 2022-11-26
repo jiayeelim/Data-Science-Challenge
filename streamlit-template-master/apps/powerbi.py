46dea30a669e0922f7ca3b12d95aa1444aa9ebb6
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2022-11-26 21:16:37 +0800

    Delete powerbi.py

--- streamlit-template-master/apps/powerbi.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.markdown(“https://app.powerbi.com/view?r=eyJrIjoiOGVhNTA1YmMtY2VhYy00Nzk4LTk3NzQtYzI4OTA5OTczNDEzIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D”,unsafe_allow_html=True)
