681715d19a28f51e6efc33955e175a3cf516081d
jiayeelim <73089478+jiayeelim@users.noreply.github.com>
2022-11-26 20:34:00 +0800

    Update home.py

--- streamlit-template-master/apps/home.py
+++ streamlit-template-master/apps/home.py
@@ -2,11 +2,6 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import streamlit.components.v1 as components
 
-
-
-    
-    
-
  def app():
     st.title("Home")
 
@@ -23,5 +18,5 @@ import streamlit.components.v1 as components
     m.to_streamlit(height=700)
 
     # embed streamlit docs in a streamlit app
-    st.markdown(“https://app.powerbi.com/view?r=eyJrIjoiOGVhNTA1YmMtY2VhYy00Nzk4LTk3NzQtYzI4OTA5OTczNDEzIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection0f765a7eda61c7c2a3c8”, unsafe_allow_html=True)
+    #st.markdown(“https://app.powerbi.com/view?r=eyJrIjoiOGVhNTA1YmMtY2VhYy00Nzk4LTk3NzQtYzI4OTA5OTczNDEzIiwidCI6IjBlMGRiMmFkLWM0MTYtNDdjNy04OGVjLWNlYWM0ZWU3Njc2NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection0f765a7eda61c7c2a3c8”, unsafe_allow_html=True)
     
